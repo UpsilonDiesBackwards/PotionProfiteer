88f5a94207b692d3029cfe9a91f38af2cc0e1e1e
KazThomas <mr.firebass@gmail.com>
2024-02-24 17:41:07 +0000

    Changed the TouchScene to have the player be dragged around
    Added moveTowards in the comment of the script but its not working idk why not

--- Assets/Scripts/TouchScreen/MoveByTouch.cs
+++ Assets/Scripts/TouchScreen/MoveByTouch.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class MoveByTouch : MonoBehaviour
 {
-
+    public GameObject Player;
+    private float moveSpeed;
     // Update is called once per frame
     void Update()
     {
-        MultiTouch();
+        SingleTouch();
     }
 
     void SingleTouch() //moves whatever has this script around the screen to wherever the finger touched the screen, can drag with this too
@@ -19,6 +20,9 @@ public class MoveByTouch : MonoBehaviour
             Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
             touchPosition.z = 0;
             transform.position = touchPosition;
+
+            // IN THEORY this line should work but its not transform.position = Vector3.MoveTowards(transform.position, touchPosition, moveSpeed);
+           
         }
     }
 
@@ -29,5 +33,10 @@ public class MoveByTouch : MonoBehaviour
             Vector3 multiTouchPos = Camera.main.ScreenToWorldPoint(Input.touches[i].position);
             Debug.DrawLine(Vector3.zero, multiTouchPos, Color.red);
         }
+    }
+
+    void TouchTowardsMove()
+    {
+        
     }
 }

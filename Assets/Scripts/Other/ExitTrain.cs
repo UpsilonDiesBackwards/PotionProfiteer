940fef4b409d663492b62c507f1c6e92e10b8b7a
KazThomas <mr.firebass@gmail.com>
2024-03-15 15:43:24 +0000

    Forgot To add Train Exit my b

--- Assets/Scripts/Other/ExitTrain.cs
+++ Assets/Scripts/Other/ExitTrain.cs
@@ -2,16 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
-public class EnterTrain : MonoBehaviour
+public class ExitTrain : MonoBehaviour
 {
-
     private void OnCollisionEnter2D(Collision2D ChangeScene) //simplest way to do it, on triggers will be the death of me - Kaz
     {
         if (ChangeScene.gameObject.CompareTag("Player"))
         {
-            SceneManager.LoadScene("TrainInterior");
+            SceneManager.LoadScene("Arena");
         }
     }
 }
-;

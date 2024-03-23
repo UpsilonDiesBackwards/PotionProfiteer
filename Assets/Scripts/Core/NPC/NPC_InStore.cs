0a15149e1f95b41dce4ee44d6523a30bfb51a9ff
KazThomas <mr.firebass@gmail.com>
2024-03-23 18:43:25 +0000

    need to add anim layers to npcs - play that layer

--- Assets/Scripts/Core/NPC/NPC_InStore.cs
+++ Assets/Scripts/Core/NPC/NPC_InStore.cs
@@ -9,15 +9,36 @@ public class NPC_InStore : MonoBehaviour
 {
     public GameObject CounterDesk;
     private float moveSpeed;
+
+    public Animator upAnim;
+    private bool isWalking = false;
+
+    private void Start()
+    {
+        upAnim = GetComponent<Animator>();
+    }
+
     // Update is called once per frame
     void Update()
     {
+        //PlayAnimation();
         MoveTowardsCounter();
     }
 
     void MoveTowardsCounter()
     {
+      
         moveSpeed = 4f;
         transform.position = Vector2.MoveTowards(transform.position, CounterDesk.transform.position, moveSpeed * Time.deltaTime);
+        isWalking = false;
+    }
+
+    void PlayAnimation()
+    {
+        isWalking = true;
+        while (isWalking)
+        {
+            upAnim.Play("WalkUp");
+        }
     }
 }

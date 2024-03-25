27b49c4471971fd441de8b16900e2586a44270f7
earthtolou <149688771+earthtolou@users.noreply.github.com>
2024-03-25 13:21:21 +0000

    adding wagon script
    work in progress :)

---
+++ Assets/Scripts/Core/Inventory/WagonInventory.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class WagonInventory : InventoryHolder
+{
+   // public UnityAction<IInteractable> OnInteractionComplete { get; set; }
+}

f8418686124579d48f9f76a432dd3f5ec716487e
Phrogy <147832040+Phrogy@users.noreply.github.com>
2024-04-01 23:08:11 +0100

    made play button script
    doesnt technically work, still figuring out why, tutorial was for an older version though so will have another bash at it tomorrow

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagment;
+
+public class MainMenu : MonoBehaviour
+{
+   public void PlayGame()
+    {
+        //this is where the code to create the save for the player should go
+        SceneManager.LoadSceneAsync("PlayerSelection");
+        //the code above is what will load the game screen
+    }
+ 
+}

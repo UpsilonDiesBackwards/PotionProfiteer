14f80e7d2fda02585439377f8b11823ed71229a8
Tayler Parsons <upsilon@diesbackwards.com>
2024-04-02 11:03:50 +0100

    Change location of MainMenu.cs script for project heirachy cohesion, correctly set up the main menu scene so that the play button properly loads you into the player customisation scene

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,11 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagment;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-   public void PlayGame()
+    public void PlayGame()
     {
         //this is where the code to create the save for the player should go
         SceneManager.LoadSceneAsync("PlayerSelection");

eb4a88ec944547710d48dc19eb055ee3e4fba4ca
KazThomas <mr.firebass@gmail.com>
2024-04-08 18:27:29 +0100

    dragging works! dropping not so much

---
+++ Assets/DragAndDrop.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(Image))]
+public class DragAndDrop : MonoBehaviour, IPointerClickHandler, IDragHandler, IEndDragHandler
+{
+    public bool isHeld = false;
+    public GameObject tomato;
+    public GameObject cauldron;
+    public void OnMouseDrag()
+    {
+        transform.position = GetMousePos(); //setting the mouse to the world position
+    }
+
+    Vector3 GetMousePos()
+    {
+        var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0;
+        return mousePos;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Instantiate(tomato, Input.mousePosition, Quaternion.identity);
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        transform.position = GetMousePos();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = GetMousePos();
+        WhereItsAt();
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        WhereItsAt();
+    }
+
+    public void WhereItsAt()
+    {
+        float proximity = 5f;
+
+        Vector3 cauldronPos = cauldron.transform.position;
+        cauldronPos.z = 0;
+        Vector3 itemPos = Input.mousePosition;
+
+        float distance = Vector3.Distance(cauldronPos, itemPos);
+
+        if (distance < proximity)
+        {
+            Debug.Log("ITEM PUT IN CAULDRON");
+        }
+        else
+        {
+            Destroy(tomato); //WILL NEED TO BE PUT BACK INTO THE INVENTORY SLOT
+        }
+
+    }
+}
+

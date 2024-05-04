a365b898b38c7d8b859daddd3c0d26b125182102
Tayler Parsons <upsilon@diesbackwards.com>
2024-05-04 11:20:10 +0100

    Bayou Swamp Water now significantly slows down the player, you can now also //sell// womp womp potions for negative profit :D

---
+++ Assets/Scripts/Other/RegionSpecific/SwampWater.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwampWater : MonoBehaviour {
+    [SerializeField] private float _speed = 2.0f;
+
+    [SerializeField] private float _playerSpeedBuffer;
+
+    void Start() {
+        _playerSpeedBuffer = Player.Instance.moveSpeed;
+    }
+
+    void OnTriggerEnter2D(Collider2D col) {
+        if (col.CompareTag("Player")) {
+            Player.Instance.moveSpeed = _speed;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col) {
+        if (col.CompareTag("Player")) {
+            Player.Instance.moveSpeed = _playerSpeedBuffer;
+        }
+    }
+}

f24167be94fe2ea9a976e37ad42445f0f8d66834
KazThomas <mr.firebass@gmail.com>
2024-05-05 10:56:50 +0100

    Added a Bin that should Destroy Emptys

---
+++ Assets/Scripts/Core/Brewing/BinIt.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BinIt : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Empty"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
